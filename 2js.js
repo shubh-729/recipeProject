4950a4d3c78690df9b27d22dbf30ed7073956ec7
Shubham Kumar <144636150+shubh-729@users.noreply.github.com>
2024-04-08 20:43:22 +0530

    first time added

---
+++ 2js.js
@@ -0,0 +1,5 @@
+// alert("hii im shubham");
+// confirm("want to share");
+// console.log("uydfsdjkedwddj");
+// document.write(" hii mot lorem34 hmfofisdmofsdkoewmjioec wemdf ewdmf,opefderfslkhfwe clcfmofdsfcergjythrrehg u");
+//
